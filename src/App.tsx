89b3c61c9addd5040e56c564a5a1a6edb6c719e6
kishaaggarwal32 <kishaaggarwal195@gmail.com>
2023-08-02 11:59:40 +0530

    added a reset button

--- src/App.tsx
+++ src/App.tsx
@@ -47,6 +47,11 @@ function App() {
   const moveTo = move => {
     setCurrentMove(move);
   };
+
+  const onNewGameStart = () => {
+    setHistory(NEW_GAME);
+    setCurrentMove(0);
+  };
   return (
     <div className="app">
       <h2>{StatusMessage}</h2>
@@ -55,6 +60,14 @@ function App() {
         squares={gamingBoard.squares}
         handleSquareClick={handleSquareClick}
       />
+
+      <button
+        type="button"
+        onClick={onNewGameStart}
+        className={`btn-reset ${winner ? 'active' : ' '}`}
+      >
+        Start New Game
+      </button>
       <h2> Current Game History</h2>
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
